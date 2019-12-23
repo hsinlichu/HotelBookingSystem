90dba84921fe45f97c6d8687d8e5728a1ec0c216
b06505031 <b06505031@ntu.edu.tw>
2019-12-23 17:21:35 +0800

    filter test

--- src/main/java/hotel/booking/controller/HomePageController.java
+++ src/main/java/hotel/booking/controller/HomePageController.java
@@ -1,8 +1,13 @@
 package hotel.booking.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import hotel.booking.container.Hotel;
 
 @Controller
 public class HomePageController {
@@ -11,4 +16,14 @@ public class HomePageController {
     	System.out.println("Home Page");
         return "index";
     }
+	public List<Hotel> FilteredHotel(List<Hotel> search,int Downfloor,int Upfloor){
+		List<Integer> price=new ArrayList<>();
+		for(int i=Downfloor;i<=Upfloor;i++) {
+			price.add(i);
+		}
+		List<Hotel> filterprice = Hotel.stream().filter(user->ages.contains(user.getAge())).collect(Collectors.toList());
+		filterAges.stream().forEach(user -> {
+		    System.out.println(user.getName()+"======"+user.getEmail());
+		});
+	}
 }
