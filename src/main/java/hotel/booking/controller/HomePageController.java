7179a4854961e78e78ebf7188693a39bbf19b235
b06505015 <b06505015@ntu.edu.tw>
2019-12-23 15:13:50 +0800

    commit test2

--- src/main/java/hotel/booking/controller/HomePageController.java
+++ src/main/java/hotel/booking/controller/HomePageController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import hotel.booking.container.Account;
+import hotel.booking.database.Database;
+
 @Controller
 public class HomePageController {
 	@RequestMapping(value={"", "/", "index.html"})
@@ -11,4 +14,27 @@ public class HomePageController {
     	System.out.println("Home Page");
         return "index";
     }
+	
+	
+	public Boolean register(String name, String email, String password) {
+		Boolean result;
+		if (password.length() <= 8) {
+			result = false;
+		}
+		if (result) {
+			System.out.println(name + " has registered successfully.");
+		}
+		return result;
+	}
+	
+	public Boolean login(String account, String password) {
+		Boolean result;
+		result = db.verifyAccount(account, password);
+		if (result) {
+			System.out.println(account + " logged in");
+		}
+		return result;
+	}
+	
+	
 }
