35e3d03c04eccd2eccc1b2d29cd18d750ac9da98
root <root@localhost>
2019-12-30 18:18:40 +0800

    remove @RequestMapping(value={"", "/", ".html"})

--- src/main/java/hotel/booking/controller/ResultPageController.java
+++ src/main/java/hotel/booking/controller/ResultPageController.java
@@ -5,11 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class ResultPageController {            //homepage·j´M§¹ªºµ²ªG­¶­±(ResultUI)
-	@RequestMapping(value={"", "/", ".html"})  //´«¦¨«ü©whtml     
+public class ResultPageController {            //homepageï¿½jï¿½Mï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½Gï¿½ï¿½ï¿½ï¿½(ResultUI)
+	  //ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½whtml     
     public String getIssues(Model model) {
     	System.out.println("Resultpage");
-        return "  ";                        //´«¦¨«ü©whtml
+        return "  ";                        //ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½whtml
     }
 
 }
