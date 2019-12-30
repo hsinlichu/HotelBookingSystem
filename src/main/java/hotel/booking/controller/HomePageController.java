3feec68ff4a96524aee0344efc7111c1eca96cfc
b06505031 <b06505031@ntu.edu.tw>
2019-12-30 16:40:28 +0800

    filter

--- src/main/java/hotel/booking/controller/HomePageController.java
+++ src/main/java/hotel/booking/controller/HomePageController.java
@@ -7,6 +7,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import hotel.booking.container.Hotel;
+import hotel.booking.container.Room;
+import hotel.booking.database.Database;
 
 @Controller
 public class HomePageController {
@@ -15,8 +17,15 @@ public class HomePageController {
     	System.out.println("Home Page");
         return "index";
     }
-	public List<Hotel> FilteredHotel(List<Hotel> search,int Downfloor,int Upfloor){
-		
+	public List<Hotel> FilteredHotel(List<Hotel> search,int Downfloor,int Upfloor,int SinNum,int DouNum,int QuaNum){
+		for(Hotel totalprice:search) {
+			db.getRoomsOfHotel(totalprice.id);
+		}
+				for(int i=0;i<;i++) {
+				if(a<=price2.get(i)&&price2.get(i)<=b) {
+						price3.add(price2.get(i));
+				}
+				}
 		
 	}
 }
