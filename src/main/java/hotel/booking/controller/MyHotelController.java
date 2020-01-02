3f67c1e93821927053651e08620af7ea8ba7ae77
b06505031 <b06505031@ntu.edu.tw>
2020-01-02 16:28:53 +0800

    build getmyhotel

--- src/main/java/hotel/booking/controller/MyHotelController.java
+++ src/main/java/hotel/booking/controller/MyHotelController.java
@@ -1,15 +1,23 @@
 package hotel.booking.controller;
 
+import java.util.*;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import hotel.booking.container.Account;
+import hotel.booking.container.Hotel;
+
 @Controller
-public class MyHotelController {               //���޲z�ۤvHotel���D��(MyHotelUI)
-	  //�������whtml     
+public class MyHotelController {               
+	       
     public String getIssues(Model model) {
     	System.out.println("Resultpage");
-        return "  ";                        //�������whtml
+        return "  ";                        
     }
+//    public List<Hotel> getMyHotel(Account account){
+//    	
+//    }
 
 }
