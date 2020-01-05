602263fdba23f0280a1123a8eb4c0f0ca914d1a1
james60708 <james60708@gmail.com>
2020-01-05 18:44:31 +0800

    result page add empty message

--- src/main/resources/static/js/result.js
+++ src/main/resources/static/js/result.js
@@ -26,6 +26,22 @@
             </a>
         </div>
         `;
+        
+        var empty = `
+            <div class="media media-custom d-block mb-4 h-100">
+                <div class="row py-2">
+                    <div class="col-lg-4 col-12 align-self-center">
+                        <img src="images/noresult.png" width="100%%" alt="Image placeholder">
+                    </div>
+                    <div class="col-lg-5 col-12 align-self-center">
+                        <div class="media-body">                           
+                            <h1 class="mt-0 mb-3">Search No Result!</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            `;
+        
         var i;
         for(i = 0;i < data.length; ++i){
         	data[i]["pic"] = Math.floor(Math.random()*23 +1);
@@ -33,6 +49,8 @@
             var tmp = sprintf(template, data[i]);
             $("div.post").append(tmp);
         }
+        if(data.length == 0)
+        	$("div.post").append(empty);
 
         //$.each(data, function (key, value) {
 
