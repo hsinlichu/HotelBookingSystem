899d0135701f772f7427f39b0ce4953b0666b1c1
root <root@localhost>
2020-01-05 12:37:19 +0800

    oder editor v1.0

--- src/main/java/hotel/booking/controller/CustomerOrderController.java
+++ src/main/java/hotel/booking/controller/CustomerOrderController.java
@@ -2,25 +2,67 @@ package hotel.booking.controller;
 
 import java.util.*;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import hotel.booking.Global;
-import hotel.booking.container.Account;
-import hotel.booking.container.Order;
+import hotel.booking.container.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import javax.validation.Valid;
+import java.util.stream.Collectors;
+
 @Controller
 public class CustomerOrderController {         
-	     
-    public String getIssues(Model model) {
-    	System.out.println("Resultpage");
-        return "  ";                        
+	@Resource(name = "loginInfoSession")
+	LoginInfo loginInfo;
+	
+	@RequestMapping(value={"ordermanagement", "ordermanagement.html"})
+    public String getCustomerOrderPage(Model model) {
+    	System.out.println("CustomerOrderPage");
+    	model.addAttribute("loginInfo", loginInfo);
+        return "ordermanagement";                        
     }
-    public List<Order> getCustomerOrder(Account account){
-    	List<Order> Orderlist=Global.db.getCustomerOrder(account.id);
+	
+	@RequestMapping(path = "/GetAllOrder", produces = "application/json; charset=UTF-8")
+    @ResponseBody
+    public List<Order> getCustomerOrder(){
+		System.out.println("GetAllOrder");
+    	List<Order> Orderlist=Global.db.getCustomerOrder(loginInfo.account.id);
+    	System.out.println(Orderlist);
+    	
+    	
+    	
     	return Orderlist;
     }
+    
+	
+	@RequestMapping(value="/editorder", method=RequestMethod.POST)
+    public ResponseEntity<?> getSearchResultViaAjax(
+    		@Valid @RequestBody @RequestParam(required = true) int id, @Valid @RequestBody @RequestParam(required = false) String Quantity, 
+    		@Valid @RequestBody @RequestParam(required = false) String dateIn, @Valid @RequestBody @RequestParam(required = false) String dateOut,
+    		@Valid @RequestBody @RequestParam(required = false) String action) {
+		System.out.println("editorder");
+        AjaxResponseBody result = new AjaxResponseBody();
+        System.out.println(id+Quantity+dateIn+dateOut+action);
+
+        //If error, just return a 400 bad request, along with the error message
+        //return ResponseEntity.badRequest().body(result);
+        return ResponseEntity.ok(result);
+	}
+
     public boolean deleteCustomerOrder(Order deleteOrder){
     	return Global.db.cancelOrder(deleteOrder);
     }
